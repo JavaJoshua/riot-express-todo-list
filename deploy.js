3a530731f0c1741c23967645e85ff1b9fd917179
Joshua <joshua.coddington@gmail.com>
2021-05-18 19:42:21 -0500

    updated ip address

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.116.163.9',
+      host: '18.188.99.17',
       username: 'ubuntu',
       privateKey: 'riot-key.pem'
     })
